e6eecd8d2b924e0a7e2ef3e58a30db4775b2f9ef
deb <deborah@canal-web.fr>
2016-10-24 14:12:40 +0200

    change collection creation to keep weight order

--- src/app/code/community/Canalweb/Slider/Model/Slides.php
+++ src/app/code/community/Canalweb/Slider/Model/Slides.php
@@ -6,16 +6,16 @@ class Canalweb_Slider_Model_Slides extends Mage_Catalog_Model_Product
         $resource = Mage::getSingleton('core/resource');
         $readConnection = $resource->getConnection('core_read');
 
-        $query = "SELECT entity_id FROM catalog_product_flat_". $storeId ." WHERE attribute_set_id=". $attributeSetId ." ORDER BY weight";
+        $query = "SELECT entity_id FROM catalog_product_flat_". $storeId ." WHERE attribute_set_id=". $attributeSetId ." ORDER BY weight ASC";
 
         $results = $readConnection->fetchAll($query);
-        $productIds = array();
-        foreach($results as $res){
-            $productIds[] = $res['entity_id'];
+
+        $collection = new Varien_Data_Collection();
+        foreach($results as $row){
+            $rowObj = new Varien_Object();
+            $rowObj->setData($row);
+            $collection->addItem($rowObj);
         }
-        $collection = $this->getCollection()
-            ->addAttributeToFilter('entity_id', array('in' => $productIds))
-            ->addUrlRewrite();
 
         return $collection;
     }
